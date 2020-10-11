4a91d5ce2eedd1a94af90b61b45e37eccebe1a90
Steven Casper <sebastiancasper3@gmail.com>
2020-10-11 00:51:27 -0400

    Use reflection to get the default name of the connected webhook

--- LiveSplit.PaceAlert/Discord/PaceBot.cs
+++ LiveSplit.PaceAlert/Discord/PaceBot.cs
@@ -1,10 +1,13 @@
-﻿using Discord.Webhook;
+﻿using System.Reflection;
+using Discord;
+using Discord.Webhook;
 
 namespace LiveSplit.PaceAlert.Discord
 {
     public static class PaceBot
     {
         public static bool IsConnected { get; private set; }
+        public static string Name = string.Empty;
         
         private static DiscordWebhookClient _client;
         
@@ -14,6 +17,13 @@ namespace LiveSplit.PaceAlert.Discord
             {
                 _client?.Dispose();
                 _client = new DiscordWebhookClient(webhookURL);
+                
+                // Webhook Client's internal IWebhook field isn't exposed in Discord.NET so we do this to get the name
+                if (_client.GetType().GetField("Webhook", BindingFlags.Instance | BindingFlags.NonPublic)?
+                    .GetValue(_client) is IWebhook webhook)
+                {
+                    Name = webhook.Name;
+                }
                 IsConnected = true;
             }
             catch

a711d17cad96898e9cead181bc5010675c694001
Steven Casper <sebastiancasper3@gmail.com>
2020-10-19 03:50:14 -0400

    Allow more than one notification per run

---
+++ LiveSplit.PaceAlert/Logic/NotificationManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using LiveSplit.Model;
+using LiveSplit.PaceAlert.Discord;
+
+namespace LiveSplit.PaceAlert.Logic
+{
+    public class NotificationManager
+    {
+        private LiveSplitState _state;
+        private ComponentSettings _settings;
+        
+        public NotificationManager(LiveSplitState state, ComponentSettings settings)
+        {
+            _state = state;
+            _settings = settings;
+
+            state.OnStart += LiveSplitState_OnStart;
+            state.OnSplit += LiveSplitState_OnSplit;
+        }
+
+        public void Dispose()
+        {
+            _state.OnStart -= LiveSplitState_OnStart;
+            _state.OnSplit -= LiveSplitState_OnSplit;
+        }
+
+        private void LiveSplitState_OnStart(object sender, EventArgs e)
+        {
+        }
+
+        private void LiveSplitState_OnSplit(object sender, EventArgs e)
+        {
+            var activeSettings = _settings.GetActiveSettings(_state);
+
+            foreach (var setting in activeSettings)
+            {
+                if (_state.CurrentSplitIndex != setting.SelectedSplit + 1)
+                    continue;
+                
+                var split = _state.Run[setting.SelectedSplit];
+                var delta = (split.SplitTime - split.PersonalBestSplitTime)[setting.Comparison];
+
+                if (!delta.HasValue) continue;
+                
+                var deltaValue = delta.Value;
+                var deltaTarget = setting.Ahead
+                    ? setting.DeltaTarget.Negate()
+                    : setting.DeltaTarget;
+
+                if (!(deltaValue.TotalSeconds < deltaTarget.TotalSeconds))
+                    continue;
+                
+                StringBuilder messageStringBuilder = new StringBuilder();
+                string[] substrings = setting.MessageTemplate.Split('$');
+                foreach (var substring in substrings)
+                {
+                    //Parse message variables
+                    if (substring.StartsWith("delta"))
+                    {
+                        string time = ToDeltaFormat(deltaValue);
+                        char negative = deltaValue.TotalMilliseconds < 0 ? '-' : '+';
+                        messageStringBuilder.Append(negative + time);
+                        messageStringBuilder.Append(substring.Substring("delta".Length));
+                    }
+                    else if (substring.StartsWith("split"))
+                    {
+                        messageStringBuilder.Append(split.Name);
+                        messageStringBuilder.Append(substring.Substring("split".Length));
+                    }
+                    else
+                    {
+                        messageStringBuilder.Append(substring);
+                    }
+                }
+
+                var messageString = messageStringBuilder.ToString();
+                if (messageString != string.Empty)
+                {
+                    PaceBot.SendMessage(messageString);
+                }
+            }
+        }
+
+        private string ToDeltaFormat(TimeSpan t)
+        {
+            if (t.Hours != 0)
+            {
+                return t.ToString(@"h\:mm\:ss\.ff");
+            }
+
+            if (t.Minutes != 0)
+            {
+                return t.ToString(@"m\:ss\.ff");
+            }
+            
+            return t.ToString(@"s\.ff");
+        }
+    }
+}
--- LiveSplit.PaceAlert/UI/PaceAlertComponent.cs
+++ LiveSplit.PaceAlert/UI/PaceAlertComponent.cs
@@ -18,7 +18,7 @@ namespace LiveSplit.PaceAlert.UI
         private LiveSplitState _state;
         private ComponentSettings _settings;
         private PaceAlertSettingsControl _settingsControlControl;
-        private bool notified = false;
+        private NotificationManager _notificationManager;
 
         public PaceAlertComponent(LiveSplitState state)
         {
@@ -26,17 +26,13 @@ namespace LiveSplit.PaceAlert.UI
             
             _settings = new ComponentSettings();
             _settingsControlControl = new PaceAlertSettingsControl(state, _settings);
+            _notificationManager = new NotificationManager(state, _settings);
             StartBot();
-
-            _state.OnSplit += PaceAlert_OnSplit;
-            _state.OnStart += PaceAlert_OnStart;
         }
 
         public override void Dispose()
         {
             _settingsControlControl.Dispose();
-            _state.OnSplit -= PaceAlert_OnSplit;
-            _state.OnStart -= PaceAlert_OnStart;
         }
 
         private void StartBot()
@@ -62,82 +58,6 @@ namespace LiveSplit.PaceAlert.UI
         public override void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
         {
             
-        }
-
-        private void PaceAlert_OnStart(object sender, EventArgs e)
-        {
-            notified = false;
-        }
-
-        private void PaceAlert_OnSplit(object sender, EventArgs e)
-        {
-            var activeSettings = _settings.GetActiveSettings(_state);
-
-            foreach (var setting in activeSettings)
-            {
-                if (notified || _state.CurrentSplitIndex != setting.SelectedSplit + 1)
-                    continue;
-                
-                var split = _state.Run[setting.SelectedSplit];
-                var delta = (split.SplitTime - split.PersonalBestSplitTime)[setting.Comparison];
-
-                if (!delta.HasValue) continue;
-                
-                var deltaValue = delta.Value;
-                var deltaTarget = setting.Ahead
-                    ? setting.DeltaTarget.Negate()
-                    : setting.DeltaTarget;
-
-                if (!(deltaValue.TotalSeconds < deltaTarget.TotalSeconds))
-                    continue;
-                
-                StringBuilder messageStringBuilder = new StringBuilder();
-                string[] substrings = setting.MessageTemplate.Split('$');
-                foreach (var substring in substrings)
-                {
-                    //Parse message variables
-                    if (substring.StartsWith("delta"))
-                    {
-                        string time = ToDeltaFormat(deltaValue);
-                        char negative = deltaValue.TotalMilliseconds < 0 ? '-' : '+';
-                        messageStringBuilder.Append(negative + time);
-                        messageStringBuilder.Append(substring.Substring("delta".Length));
-                    }
-                    else if (substring.StartsWith("split"))
-                    {
-                        messageStringBuilder.Append(split.Name);
-                        messageStringBuilder.Append(substring.Substring("split".Length));
-                    }
-                    else
-                    {
-                        messageStringBuilder.Append(substring);
-                    }
-                }
-
-                var messageString = messageStringBuilder.ToString();
-                if (messageString != string.Empty)
-                {
-                    PaceBot.SendMessage(messageString);
-                }
-
-                notified = true;
-            }
-        }
-
-        private string ToDeltaFormat(TimeSpan t)
-        {
-            //TODO: The existence of this method is stupid
-            if (t.Hours != 0)
-            {
-                return t.ToString(@"h\:mm\:ss\.ff");
-            }
-
-            if (t.Minutes != 0)
-            {
-                return t.ToString(@"m\:ss\.ff");
-            }
-            
-            return t.ToString(@"s\.ff");
         }
 
         public override Control GetSettingsControl(LayoutMode mode)

0072fa1683bb90b5f3ed5f5988cea965fa102b32
Steven Casper <sebastiancasper3@gmail.com>
2020-12-13 16:26:56 -0500

    Release 0.5.0

--- LiveSplit.PaceAlert/Properties/AssemblyInfo.cs
+++ LiveSplit.PaceAlert/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("C168A712-F932-4D11-8D73-A8DBFBC81116")]
-[assembly: AssemblyVersion("0.4.1.0")]
-[assembly: AssemblyFileVersion("0.4.1.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
